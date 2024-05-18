b417fd536f6337cd80f0195c9bb1f0afc3a40a3c
chanming1920 <63197820+chanming1920@users.noreply.github.com>
2024-05-19 00:01:29 +0900

    Add files via upload
    초기

---
+++ Main.js
@@ -0,0 +1,29 @@
+class PassWord
+{
+    constructor()
+    {
+        function getPassWord()
+        {
+
+        }
+
+        function changePassWord()
+        {
+
+        }
+        function searchPassWord()
+        {
+
+        }
+        function alarmPassWord()
+        {
+
+        }
+    }
+}
+
+class String
+{
+    arr = [];
+}
+
