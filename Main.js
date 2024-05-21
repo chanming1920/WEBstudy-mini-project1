f71e48ecaa446dcaf48a0e8d33c3ff457d5ca82f
chanming <hyecop9523@naver.com>
2024-05-21 18:12:30 +0900

    1. 패스워드 내 문자타입 판별 추가 2. 패스워드 정책 중 숫자 룰 추가
    3. 메인 HTML 개선 필요
    4. 추가 기능 구현 필요

--- Main.js
+++ Main.js
@@ -10,8 +10,35 @@ data2.addEventListener('click',
         function Research(){
             const str = stringData;
             const arr = str.split('');
-            arr.foreach()
-        }
+
+            const checkArr =  countDifferentTypes(arr)
+            //arr.foreach()
+            for(var i=0; i <= arr.length; i++)
+                if(typeof arr[i]  === Number && typeof arr[i+1] === Number && typeof arr[i+2] === Number)
+                    var j0 = parseInt(arr[i]);
+                    var j1 = parseInt(arr[i+1]);
+                    var j2 = parseInt(arr[i+2]);
+                    if((j0 == j1 && j0 == j2)  || (j1 - j0 == 1 && j2 - j0 == 2))
+                        alarm();
+        }    
     }
 );
 
+// 알람용
+function alarm()
+{
+    console.log('경고입니다.');
+}
+
+// 해당 함수로 문자 종류 판단
+function countDifferentTypes(array) {
+    const types = {};
+
+    
+    array.forEach(item => {
+        const type = typeof item;
+        types[type] = (types[type] || 0) + 1; 
+    });
+
+    return Object.keys(types).length; 
+}
